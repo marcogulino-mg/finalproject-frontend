9a13c318ff33cd4af445573fc5cde17551162d18
marcogulino-mg <marcogulino@gmail.com>
2025-07-22 17:17:41 +0200

    feat(header):   - improved header component design

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Main from './components/Main'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
 export default function Header() {
     return (
-        <header>
-            <h1>Header</h1>
+        <header className="d-flex justify-content-between align-items-center px-4 py-3">
+            <figure>
+                <img src="/logov2.png" alt="logo"/>
+            </figure>
+            <nav className="menu">
+                <ul className="nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Videogames</a></li>
+                    <li><a href="#">Consoles</a></li>
+                </ul>
+            </nav>
+            <div className="searchbar">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
         </header>
     )
 }
