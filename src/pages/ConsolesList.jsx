142be5152aa0291f6a6a97544d5707c37d2048c0
marcogulino-mg <marcogulino@gmail.com>
2025-07-25 16:31:34 +0200

    feat(console):   - added message when search query doesn't give results
    feat(videogame):
      - added message when search query doesn't give results

--- src/pages/ConsolesList.jsx
+++ src/pages/ConsolesList.jsx
@@ -5,5 +5,9 @@ import ConsoleCard from '../components/consoles/ConsoleCard';
 export default function ConsolesList() {
     const { consoles } = useContext(SearchContext);
 
+    if(consoles.length <= 0){
+        return <div className="container-xl d-flex justify-content-center vg-list-container mt-4"><h2>No consoles match your search criteria</h2></div>
+    }
+
     return <ConsoleCard cslist={consoles}/>;
 }
--- src/pages/VideogamesList.jsx
+++ src/pages/VideogamesList.jsx
@@ -5,5 +5,9 @@ import VideogameCard from "../components/videogames/VideogameCard";
 export default function VideogamesList() {
     const { videogames } = useContext(SearchContext);
     
+    if(videogames.length <= 0){
+        return <div className="container-xl d-flex justify-content-center vg-list-container mt-4"><h2>No video games match your search criteria</h2></div>
+    }
+
     return <VideogameCard vglist={videogames} />;
 }
